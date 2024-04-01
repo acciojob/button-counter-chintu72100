46c47e771fa702db295f16198440bf5633f493df
Chintu jha <67228053+chintu72100@users.noreply.github.com>
2024-04-02 01:27:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [val, setVal] = useState(0);
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {val} times</p>
+        <button onClick={()=>setVal(()=>val+1)}>Click Me</button>
     </div>
   )
 }
